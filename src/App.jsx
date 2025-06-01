7c8de570d36d2cfaade1e16af5c11f2fc972fc02
Damir1401-blip <211155035+Damir1401-blip@users.noreply.github.com>
2025-06-01 21:42:46 +0000

    added more sounds and updated supporters list

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,15 @@ https://www.myinstants.com/media/sounds/sad-meow-song.mp3" img="https://media1.g
         <MusicButton link="https://www.myinstants.com/media/sounds/eagle-screech-caw-sound.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNm8xdTVuaDUxcTdxMG5sazRtYTR0OTM5OHM5c3I2cnV5Njh1c3FqbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2jHWNTJVdkdFK1ZpJk/giphy.gif" />
         <MusicButton link="https://soundbuttonsworld.com/uploads/7734a0d8-d26f-49b1-9b87-45e8272b1183.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnNnazl2ZWJvdXB3MXAwa2VuZWpkNzlzaGR3eGcxbzg5b2x4eWlmdiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/May0SdjFNSrckK7LO9/giphy.gif" />
         <MusicButton link="https://www.myinstants.com/media/sounds/anime-wow-sound-effect.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXY0aWF4bmx1MTdwbjFobW1lazJqdzF1b2w5Z2I0dDIzNjlndWtzYyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YyQk8mzP146ZdhlWKT/giphy.gif" />
-        <MusicButton link="https://drive.google.com/uc?export=download&id=1HNey_1xAswljcuoVuU0PQakEsuQx-7ry" />
+          <MusicButton link="https://www.myinstants.com/media/sounds/baby-laughing-meme.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExanp5a3Y5b3l1Nmd2cmptZmYwbjdmZmMzdHR3Y2FxaW54aGZhbjY5ZSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/L0SaMZxxqMgsAJf69v/giphy.gif" />
+            <MusicButton link="https://www.myinstants.com/media/sounds/jove-lovi-aptechku-brat-blia.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExam1yeTJ4MGtvc3o2bTBodGFhN2g5ZjJjcGZsZzczcjEwbHczOGU4MiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MaZiCmnaOnXlvbeqIF/giphy.gif" />
+            <MusicButton link="https://www.myinstants.com/media/sounds/bombardino-crocodilo_MSpHN9M.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3NqMjF3Mng5NjY4eDZ6cnZxbWQ5bWp2Ymo5bjFlZTFyMXhvbjVwdCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/JtcTuWWhY3uXpSuwYG/giphy.gif"  />
+           <MusicButton link="https://www.myinstants.com/media/sounds/tralalero-tralala-meme_R8mqoQo.mp3" img="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDd5YnptMGtuODI3MHNoZjBtZ2N1eDF5OHpzOXZya2Z1NTUwcGRpaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LdZD8NIcNAPUxoyftZ/giphy.gif" />
+           <MusicButton link="https://www.myinstants.com/media/sounds/capuccino-assasino-italian-brainrot.mp3" img="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGo2cTg2ZjhvNjhvNGpzN2t2MnR3ZW8xZWttZnNqZzV1a2V1dWdpNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Xg6Xp3qLRpNKxiO5ns/giphy.gif" />
+          <MusicButton link="https://www.myinstants.com/media/sounds/bobrito-bandito-italian-brainrot.mp3" img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWJkdzBkMTJ5c2kwOGNxMnhjcDF4cDdmM2I3aGVyZ2Jsdmk4bXMxdSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/eCAYGZg0Dev5zLvxC2/giphy.gif" />
+          <MusicButton link="https://www.myinstants.com/media/sounds/tung-tung-tung-tung-sahur.mp3"    img="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTIzemtkMDF2cGlhZmZsd3NjbzcxZDlrejVrOXYybjc2YmEzM2Q1NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/SfZZnC4IQoR77rTzlD/giphy.gif" />
+
+
         <Supporters />
       </>
   )
--- src/Supporters.jsx
+++ src/Supporters.jsx
@@ -10,8 +10,8 @@ function Supporters(){
             <img className="img1" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYndjMjh4dXdrY2d0NjB0a2Jzcng5aG1hbzQyZGM5b251NzltMDNpciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/diAhf8bYer76E/giphy.gif"
             />
             <ul className="supporters">
-                <li></li>
-                <li></li>
+                <li>Сева поросенок</li>
+                <li>Я сам</li>
                 <li></li>
                 <li></li>
                 <li></li>
